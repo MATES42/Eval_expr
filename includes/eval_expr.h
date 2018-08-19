ca2be4a1d988c5fd175549cb56528c364814e15a
Jonathan CHIRK <jchirk@e3r4p19.42.fr>
2018-08-19 17:41:53 +0200

    Correction struct

--- includes/eval_expr.h
+++ includes/eval_expr.h
@@ -6,7 +6,7 @@
 /*   By: jchirk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/19 16:58:45 by jchirk            #+#    #+#             */
-/*   Updated: 2018/08/19 17:40:13 by jchirk           ###   ########.fr       */
+/*   Updated: 2018/08/19 17:41:22 by jchirk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ typedef struct	s_oper
 
 typedef struct	s_result
 {
-	t_token		**input;
+	t_token		*input;
 	int			size;
 }				t_result;
 
